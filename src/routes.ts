bdb638e64536c07868038c587d61be5f8bdf9506
Angelica dos Santos <santosangelicassp@gmail.com>
2023-08-30 09:48:54 -0300

    [CREATE] GetItem endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { berriesRouter } from "./modules/berries/berries.routes";
+import { itemRouter } from "./modules/item/item.routes";
 
 export const appRoutes = Router()
 
 appRoutes.use('/berries', berriesRouter)
+appRoutes.use('/item', itemRouter)
